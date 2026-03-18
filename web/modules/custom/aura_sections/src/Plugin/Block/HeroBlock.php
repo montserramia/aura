b2b7ab5446155f2166766294403f588934454a25
montserramia <montserramia@users.noreply.github.com>
2026-03-18 18:58:27 +0100

    poster de hero

--- web/modules/custom/aura_sections/src/Plugin/Block/HeroBlock.php
+++ web/modules/custom/aura_sections/src/Plugin/Block/HeroBlock.php
@@ -25,8 +25,9 @@ class HeroBlock extends BlockBase {
       'subtitle' => $this->t('Un procés pensat perquè passis de la idea a la pràctica amb seguretat.'),
       'button_text' => $this->t('Parlem-ne amb calma'),
       'button_link' => '#contact',
-      'background_video' => '/themes/custom/aura_theme/video/hero-1.mp4',
-      'background_video_poster' => '/themes/custom/aura_theme/images/frame000.png',
+      'background_video' => '/themes/custom/aura_theme/video/hero-1.webm',
+      'background_video_poster' => '/themes/custom/aura_theme/images/      {% set poster_fallback = background_video_poster|default(base_path ~ 'themes/custom/aura_theme/images/20260318182325.png') %}
+.png',
     ] + parent::defaultConfiguration();
   }
 
